9fade167d7a769c288d470e489d2e8531e3f4080
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2026-03-01 22:34:00 -0300

    AtualizaçAo de url e image dos itens

--- script.js
+++ script.js
@@ -1,103 +1,98 @@
 const ItensLista = [
     {
         Nome: "Ferro de passar roupa",
-        imagem: "https://images.unsplash.com/photo-1517639493569-5666a7b2f494?w=500",
-        url: "https://www.google.com/search?q=ferro+de+passar+roupa"
+        imagem: "https://m.media-amazon.com/images/I/41nRdAvKM9L._AC_SL1000_.jpg",
+        url: "https://www.amazon.com.br/Decker-Vaporizador-Sistema-Antigotejamento-FX3100/dp/B083ZLZGF7/ref=pd_ci_mcx_mh_mcx_views_0_title?pd_rd_w=F0ZzF&content-id=amzn1.sym.87e935c8-d687-4dd4-9dbf-8b9bdbb5018e%3Aamzn1.symc.c3d5766d-b606-46b8-ab07-1d9d1da0638a&pf_rd_p=87e935c8-d687-4dd4-9dbf-8b9bdbb5018e&pf_rd_r=WHZYYX0HDN79GSJP59KE&pd_rd_wg=zGQhK&pd_rd_r=79e6556a-4698-4906-baf9-efe834e20dfe&pd_rd_i=B083ZLZGF7"
     },
     {
         Nome: "Conjunto de Panelas",
-        imagem: "https://images.unsplash.com/photo-1584990344119-a2ce1082fab5?w=500",
-        url: "https://www.google.com/search?q=conjunto+de+panelas"
+        imagem: "https://m.media-amazon.com/images/I/61GS-3FA5vL._AC_SL1000_.jpg",
+        url: "https://www.amazon.com.br/Panelas-Tramontina-Antiaderente-Turim-Vermelho/dp/B0BBS9KDHT/ref=sr_1_26?crid=3E0D95IQNY1OL&dib=eyJ2IjoiMSJ9.x9bVPJW7VZFOBDz6ZMV1vtgz1mIWrkElkkJO8_buW2lwdb-tGViIYDakgXrp-1R6nc7LmX7sdUxBKkor3uMuFzV7-MtzQVKLzMI6ish11S-uPLCZv8eOGNP_fk7tgUh8OsV3xVFRC21BH14hPkcasGXuXERwTqqk4Wmgm9vDfeNP4QtXTm6ZAdK7z3XeP65uxcmar2HllRXI-824qXsZyM0Gt834dg1rKGMLvJUEQBGqwX8dJY14LMixzitnZVp8NIb2Ay9EgtfwbZ9RuLttOAZVcfvOiG9ursEg7VFrEtE.Pkxa6RyHO1cRGQIF9J-baygRlxihh_0ejdqUQX5KAis&dib_tag=se&keywords=conjunto+de+panelas+tramontina&qid=1772413796&sprefix=conjunto+de+panelas%2Caps%2C265&sr=8-26&ufe=app_do%3Aamzn1.fos.4bb5663b-6f7d-4772-84fa-7c7f565ec65b"
     },
     {
         Nome: "Panela de Pressão",
-        imagem: "https://images.unsplash.com/photo-1590794056226-79ef3a8147e1?w=500",
-        url: "https://www.google.com/search?q=panela+de+pressao"
+        imagem: "https://m.media-amazon.com/images/I/71vhS1jZqnL._AC_SL1500_.jpg",
+        url: "https://www.amazon.com.br/Tramontina-Vancouver-Alum%C3%ADnio-Revestimento-Antiaderente/dp/B0CD4SFMD7/ref=sr_1_9?dib=eyJ2IjoiMSJ9.hhznWoGHskEmpRz3HdNScrc8-cn87-uWj_avRDWi_bXJqE5SR7A4z_a3GaW7K1fGIHsWbcGOY1NTHxhMuYWu6WSSc9dPMRsUMFh6Lb5igBFXHzqriG322k-uMtVE4myVddO4GwQ2DU-MIyQ8krrUzZnYMd4m48cPuZyxuIQS1g9qSlrH6o1OiXRigfxJGnO2vhpbd0I7o5sHPnH-BVld_8o3VoCboZSslXJ-E6_9qMQhReWV2QQtsbPyZvWEP01iylBqq7RfYx_2wTlqn7uhtogetY3dnNgzCNCIzSkMJI4.RI49CzgRosz4xufSaDjw-S1bJnV4By8KEY4mTxBYB9U&dib_tag=se&keywords=panela+de+press%C3%A3o&qid=1772413707&sr=8-9&ufe=app_do%3Aamzn1.fos.fcd6d665-32ba-4479-9f21-b774e276a678"
     },
     {
         Nome: "Aspirador de Pó",
-        imagem: "https://images.unsplash.com/photo-1558317374-067fb5f30001?w=500",
-        url: "https://www.google.com/search?q=aspirador+de+po"
+        imagem: "https://m.media-amazon.com/images/I/31RDw6Caq9L._AC_SL1000_.jpg",
+        url: "amazon.com.br/Aspirador-Vertical-1300W-STK15-127V/dp/B094X2N4HV/ref=sr_1_5?crid=4PEF1D2PQ123&dib=eyJ2IjoiMSJ9.BDCZjXJftGC3UuoDPi3O_RsLnwaJOrReyoZsVLWEQXE1shBEgR0LdUHi8nC-SPg3FNkGfw7cWRmdIQZQKIVKnsgueR24DOheIbC9ZnqvV1Poc7slq8E1nEyJFolavYyO7utyy5bqu1mMFVDshF2fQSIRguPL7H7zkrkkJriOzODps2ABxmcVXKELWefX5Y2UXdJ4jYfAJzUKkdA3CFOpE4ddoRUid8wI91lkDbdz07tXM43vyve89uz7QCwJUrnkgBeZUtN60IYon9nBqnNBfjOVVLL5x2DEPDo0HGnW2zQ.X6hQPzBPWd6lDPM7u2Vkv-CTDv3eMEoyQi3WilLaaos&dib_tag=se&keywords=aspirador+de+pó&qid=1772414083&sprefix=aspira%2Caps%2C382&sr=8-5&ufe=app_do%3Aamzn1.fos.fcd6d665-32ba-4479-9f21-b774e276a678"
     },
     {
-        Nome: "Caminha de Gato",
+        Nome: "Caminha de João Pedro",
         imagem: "https://images.unsplash.com/photo-1548546738-8509cb246ed3?w=500",
         url: "https://www.google.com/search?q=caminha+de+gato"
     },
     {
         Nome: "Assadeira de inox/vidro",
-        imagem: "https://images.unsplash.com/photo-1581439645268-ad7bb4df3532?w=500",
-        url: "https://www.google.com/search?q=assadeira+de+inox+vidro"
+        imagem: "https://m.media-amazon.com/images/I/51MNOC0GgPL._AC_SL1000_.jpg",
+        url: "https://www.amazon.com.br/Tramontina-61310341-Assadeira-Inox-Prata/dp/B076JKHSWL/ref=sr_1_8?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2DH0ESG2RXFKB&dib=eyJ2IjoiMSJ9.WMS5WC-9pSKKiA_aHAwRGcSWjB5UZxVyIyf2Hkc6SYI0Z-NrcCpG4Gu_BCGllCYI4hFvPa66okshcKxKWIuwfoOi0NZJBtSSrzWDwb7Gyz-qo8BMm2_hwPUQxMa3jNOYXJ8JDsRb6QoJsd43Ir9tvDiGw1hknWcC9E92XfAyXiOrrgz_HNvSweFd7mUsqYBvxt9pVQVOM1ZQXVDNeYG1zsATmpWfPMMJTnJlWCJDBK2ttivKvg3J9d1y_GGsysKS2zIrWtaePxT_vR3lUl8Yo2DAD_DAomNZX0uvdPkIiFY.hqZ06_vnYI1eKLOFrUUQMzAILvkMTL7nzShsJCQiPHs&dib_tag=se&keywords=assadeira%2Bde%2Binox&qid=1772414402&sprefix=assadeira%2Bde%2Binox%2Caps%2C241&sr=8-8&ufe=app_do%3Aamzn1.fos.a492fd4a-f54d-4e8d-8c31-35e0a04ce61e&th=1"
     },
     {
         Nome: "Cafeteira elétrica",
-        imagem: "https://images.unsplash.com/photo-1517668808822-9ebb02f2a0e6?w=500",
-        url: "https://www.google.com/search?q=cafeteira+eletrica"
+        imagem: "https://m.media-amazon.com/images/I/71cM7xwJqXL._AC_SL1500_.jpg",
+        url: "amazon.com.br/Electrolux-ECM10-Cafeteira/dp/B09CC72K1X/ref=sr_1_6?crid=1CET220WXNN6B&dib=eyJ2IjoiMSJ9.Jm4XjDECmVPQNSfeDnzepTkKOpHnYoFjeWB8H356u4w87vLcJ8Tby8ytFRWstf3DzAOYU3KMhxroM_zzxx-PMJbJDSZeOiNVDQqsn4WqtQ32WyBEbA2lVYFk206uAu6kS_zLD120tNN_gZGpLVNU8n8Qg7SCLq6S63TIBxRuCRIJ_Pv42FC889gvwmarnu4E21CwCMVEWUvru9H72_LBbFAsIs0rxLdGN136QiVIX_Ksq05Hp3qS4oafEWaeYd0QpMOt7_UgchCYOki0fwSOSUI3GiAXmC-CHtr9Ogrh_yI.2lfGmscmHpNEpWA-Kgpb9I0a3NWvJtzPHPaN-9gpXNQ&dib_tag=se&keywords=cafeteira+eletrica&qid=1772414438&sprefix=cafeteira+%2Caps%2C265&sr=8-6&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d"
     },
     {
         Nome: "Jogo de Bowls Inox",
-        imagem: "https://images.unsplash.com/photo-1610348725531-843dff563e2c?w=500",
-        url: "https://www.google.com/search?q=jogo+de+bowls+inox"
+        imagem: "https://m.media-amazon.com/images/I/41C6AViu6ZL._AC_.jpg",
+        url: "https://www.amazon.com.br/Conjunto-Tigelas-Mixing-TopChef-BOWL18-20-22-24-26-28cm/dp/B0DSCKZD9D/ref=sr_1_7?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2TTB7ILWBSM83&dib=eyJ2IjoiMSJ9.g2veUFTIENs0xqRCxqtbxr3C6pfLYaNSO1Gbae-0OYIOwoS8-Mtqro0bR073hoq3094ZgV6YBFiqOUcCCdq-VD0fyiNX0R98kK84Ma4P3o-ka_g8fcRT2niGym7JIIgxt7N-7MGsGQOtQoz7ULyIK7xhbTpnRePamSNm-e3_10_djOuNachmW42Tdmj7nqSWYJNltMPlVceRoCEYsjwcJgqYsTQz29w-b66UTU2pkeb1W08yg3pOpdt9fesJvmXvaPFQvfsw-bxArnkScfHcItJVkV8zmahh3H284Zxrkbw.KUmpb8b2m3FSa2FF_qFuO9ih-Kzzky9wlTBEx24haCE&dib_tag=se&keywords=jogo%2Bde%2Bbowl%2Binox&qid=1772414507&sprefix=jogo%2Bde%2Bbowls%2Binox%2Caps%2C265&sr=8-7&ufe=app_do%3Aamzn1.fos.6a09f7ec-d911-4889-ad70-de8dd83c8a74&th=1"
     },
     {
         Nome: "Jogo de Talher",
-        imagem: "https://images.unsplash.com/photo-1551326844-4df70f78d0e9?w=500",
-        url: "https://www.google.com/search?q=jogo+de+talher"
+        imagem: "https://m.media-amazon.com/images/I/61x2HGF7CfL._AC_SL1000_.jpg",
+        url: "https://www.amazon.com.br/Faqueiro-Inox-Buzios-Tramontina-23799/dp/B0789M146B/ref=sr_1_5?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LT939IBMWCE1&dib=eyJ2IjoiMSJ9.s3TZ3NmdtUGxTS7HJk65MI104zOLjCqme-O_5mvAh_nBlaACNJbDB_sdWyKYDTixaIJtAvKZvMnLUw3sRLkE5MztQSFcf50F3l50xAIo1_oM9p18GXWFTbzLV6IZbj6OO1ZDaZS7jb7UyWpMr99lDotSre8xMr5NPNv_xi3r_ec-cHzOhaeUgX8-5gRKTxdrbB5NmovuV_e0eAuZHrg1fxDykR0MNObzKus2R7_9AEne1HvMrCLAJGuSHZjlE9zRnD5TyMtNc3F2eXoUzfQ4rhbAwm-zBRFMm0Ua303jnWI.GOOWl0rceXsYmZUFUazE3Lry3m7q6PtwS0IX8iOdCEs&dib_tag=se&keywords=jogo+de+talher+inox&qid=1772414557&sprefix=jogo+de+talher+in%2Caps%2C268&sr=8-5"
     },
     {
         Nome: "Escorredor de Pratos Inox",
-        imagem: "https://images.unsplash.com/photo-1622262512638-7613768340d2?w=500",
-        url: "https://www.google.com/search?q=escorredor+de+pratos+inox"
+        imagem: "https://m.media-amazon.com/images/I/61qciphpIlL._AC_SL1200_.jpg",
+        url: "https://www.amazon.com.br/Escorredor-Pratos-Porta-Talheres-Inox/dp/B0786S11XL/ref=sr_1_6?crid=31C9G0VK8CH50&dib=eyJ2IjoiMSJ9.wnlRKX3RmFTGnXbviJCSQ84T3HkybtCsBEsER82itBqYz5Gv0XEK__cH4BeFK6XmrC7ZL_ZhginQYDmN1SrApBwa5DzG65FPtuAZaIzUa6cwgcERy-zSe17XDlClm8-FHA6j6MIL925c6mqKMeD4-NEfTw7nJ67wPNyVO0N1hHNcwIMpBRIjcMwKDXLA6HXIn7N1qBSdto5DBkj5MzVaov5Aj2pQ8GV6rocFUbHFnEitMkM3Il9Xlq_WhnN0wkHIPw4-HyQsqmwXxrGTZQIxfK2oqK_0VJ1wE5AKN0WAKH0.YRT_y5IWdfiSZ5LWLMPWxHfGVOysjp81yTzVFh-nX3s&dib_tag=se&keywords=escorredor+de+pratos+inox&qid=1772414644&sprefix=escorredor+de+pr%2Caps%2C270&sr=8-6&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d"
     },
     {
         Nome: "Sanduicheira",
-        imagem: "https://images.unsplash.com/photo-1610439003310-449e5d415f3e?w=500",
-        url: "https://www.google.com/search?q=sanduicheira"
+        imagem: "https://m.media-amazon.com/images/I/61eGDtwre7L._AC_SL1500_.jpg",
+        url: "amazon.com.br/Sanduicheira-Master-Grill-Inox-S-20/dp/B084XR9K4X/ref=sr_1_9?__mk_pt_BR=ÅMÅŽÕÑ&crid=2ZO1H0TAXONTC&dib=eyJ2IjoiMSJ9.r7TEi1_6CBOekdrAf7WwfjdKtB26yHmV8q2gusIJS6p5EE6ccS13RBMEekgqAC3Q2pApDT2YvBpEgmwaEU3rPwJVmrfNH7VerU2D3U8bIg_B1ekkLodgD4W7AuK3ein1mIRrxBlNQ1VSyFWFG2_AvmHIhCEOsLuI9KcOfQBYiZJq0X3LJZqR1Q0KOXsjRbRLQqpPOEQOOdur2iQJZOt1ECGZ23-8z-AERL3kMViRd3HUclrnph6L8egFhDconQhkJr6y9vefHEbsuiHD2j0mSD4C7WI0QHoBa_SwA5Fy6fI.B9buu29dFD0rVxVQeSMUsCgu07R_NvFGFQinD4u_W-c&dib_tag=se&keywords=sanduicheira+eletrica&qid=1772414697&sprefix=sanduicheira+eletric%2Caps%2C273&sr=8-9"
     },
     {
-        Nome: "Kit 4 Taças de Vinho",
-        imagem: "https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?w=500",
-        url: "https://www.google.com/search?q=kit+tacas+de+vinho"
+        Nome: "Kit Taças de Vinho",
+        imagem: "https://m.media-amazon.com/images/I/51cNZIWhjjL._AC_.jpg",
+        url: "amazon.com.br/Cristal-Conjunto-Unidades-Brancos-Millilitros/dp/B0GK9RDPJM/ref=sr_1_8?__mk_pt_BR=ÅMÅŽÕÑ&crid=1CP6PC6V4MTDV&dib=eyJ2IjoiMSJ9.G68zz1c6-KPQ5oPPgKYjsZMUC5F-NkcXnBRMVkVEAr9g5PLGsxTYoXV-YqAks3n-z76QGZR-OeQCHhWLU8Zt_b3-UHAqp2vr7GG93nsUf_f96YBznh9VNoHk3JhCE-neM0my-TP8DfmfODUarR_9nS07e-8QaMlBNUELPp4L7-qNBF3YTtmUqP-VE1wpLOSUPFyaai5o39eb6mg1i_XEaVHvUKXOJH_XIbQJxHdTB7W71QjIblKdkRbHXRylvn2UpHzR4YhnRYDW4V-C37JP2Y5CH7soT-R5W5sMSNugQjI.oa5-6YoKL_4DczYTAu9-UWqxzY5yOR67EnBOjNLikaU&dib_tag=se&keywords=Kit+taça+de+vinho&qid=1772414739&sprefix=kit+taça+de+vin%2Caps%2C260&sr=8-8&ufe=app_do%3Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9"
     },
     {
         Nome: "Kit de Utensílios Cozinhar",
-        imagem: "https://images.unsplash.com/photo-1593113598332-cd288d649433?w=500",
-        url: "https://www.google.com/search?q=kit+utensilios+cozinha"
+        imagem: "https://m.media-amazon.com/images/I/51RDO+bHUiL._AC_SL1000_.jpg",
+        url: "https://www.amazon.com.br/Utens%C3%ADlios-Cozinha-Silicone-Resistente-Antiaderente/dp/B0CKWDY274/ref=sr_1_6?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=36WBK2LJFTKO7&dib=eyJ2IjoiMSJ9.Av8zOjXDTdB45sItMkdnIrYup2-EGTlqGJ8tlYQS33I9kjn_rmdxCNp3ulPvSpXNKhuV-_oJHspk_3Pk6_F6gulnBHcPIIMCLeBF5JQvB5mbqP_7MehwMELpkbonAmOtz-cLnUlnnUXO1X-DdJaUBztCwNr0zAxr-_YtbVe9MNwzCiSwc8s6ju84_5eGsggO-t4Rvz-fokrZOyOgF6cC501OrQQHHVAnMYVKZ744rgfGQnZ8WKTAt0O4A3hEwwcZPIxJdOyBbLPJBPeD069CtY4JSo5Tfmw5FFuSwGr44hA.eWij_HmWAHTeRXUgtVebxc9dhz9qo3xc-JrTT7rdNdg&dib_tag=se&keywords=jit+utensilios+de+cozinha&qid=1772414795&sprefix=kit+utensilios+de+cozinh%2Caps%2C248&sr=8-6"
     },
     {
         Nome: "Jogo 10 Potes Vidro (Marmita)",
-        imagem: "https://images.unsplash.com/photo-1584263347416-85a696b4eda7?w=500",
-        url: "https://www.google.com/search?q=potes+de+vidro+marmita"
+        imagem: "https://m.media-amazon.com/images/I/6141QRR9kyL._AC_SL1000_.jpg",
+        url: "https://www.amazon.com.br/Borossilicato-Retangular-Marmita-Herm%C3%A9tica-Resistente/dp/B0GFFJ36NG/ref=sr_1_6?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=35W9OZHMV3N42&dib=eyJ2IjoiMSJ9.tybJrVTRs2vfKSJN44vbiNJAEd8xFDTgFGAvknVyrJ5wPh-3bBTgFb4M7p4-XdTGZeAu4CdPqDUD67qeqVU1C78KcZjyTayjxItOznh7YNlvU0bkRx-QvwJFM33OYVxcSye11YWcKdEYtpIb6CQfZEQ6x8nV3LeI6ckNRQ9zUEw_AfN9osXsV4CaaYzEDDmRRcmO-AJ8KtAb7gD0Kh_rBBUBWElPg2rV7YJ9FVabg5brUM_ur3DYlqrIDlipQDcwQCaf1-UEhNPuisahcEZsXoS1gwx3iJ9XHSvUp0wzU3c.pV2WNtz8IMvpMJNPvaMi5tQt6ZNFeuoG1V2O536kMqM&dib_tag=se&keywords=jogo+10+potes+de+vidro&qid=1772414888&sprefix=jogo+10+potes+de+vid%2Caps%2C255&sr=8-6&ufe=app_do%3Aamzn1.fos.a492fd4a-f54d-4e8d-8c31-35e0a04ce61e"
     },
     {
         Nome: "Conjunto Peneira Inox",
-        imagem: "https://images.unsplash.com/photo-1604329760661-e71dc83f8f26?w=500",
-        url: "https://www.google.com/search?q=peneira+inox"
+        imagem: "https://m.media-amazon.com/images/I/51E5IJI0JQL._AC_SL1300_.jpg",
+        url: "https://www.amazon.com.br/Conjunto-Peneiras-Coador-Resistentes-Cozinha/dp/B0D4JMMJ9V/ref=sr_1_6?crid=2KKYZL5WK17L&dib=eyJ2IjoiMSJ9.Kqi3_yduFkikh1W8UAPahKDeYjGhRBjgPJmnae4kJU73RHf8Fh5Ki30_KW2Tm_gFlMrfIXY9wERYjRaLb989Wm7dRBkP1uMVHUOpN-iVJLJhcZEUjE-WHAmqUfA-Vz3moc9JSPeFYCLzSQTstAdY7w8ZYtwe7DZQfnUgaLaPRQAh99DtxPVbZBJ6gTq4lqbDcj9z_0Cq5S9otQkTpDkUPYz34mPhNm1YrM7iKlBS_1tX51caizhmtB88q7T9g9BNA6AX_4GQaH0Qd0cQwgu_duOYT2Rm8rvVKEAn3xvx2Eo.IkY25IEnkWnfZkwuE5orKJFDZ-uu27t5cFEEEN8gaiI&dib_tag=se&keywords=conjunto+de+peneira+inox&qid=1772414934&sprefix=conjunto+de+pene%2Caps%2C267&sr=8-6"
     },
     {
         Nome: "Jogo 4 Toalhas de Banho",
-        imagem: "https://images.unsplash.com/photo-1560361100-33827179069d?w=500",
-        url: "https://www.google.com/search?q=jogo+toalhas+banho"
+        imagem: "https://m.media-amazon.com/images/I/71s-kG60G+L._AC_SL1500_.jpg",
+        url: "https://www.amazon.com.br/Toalhas-algod%C3%A3o-poli%C3%A9ster-Teka-Monaco/dp/B0FPDBXGGT/ref=sr_1_9?crid=ZQNRSMTCSY6V&dib=eyJ2IjoiMSJ9.nl5loG2kAuHfz0_rACPaATj7RfjhM0_92hXbOxP0Kk35o-f6w4gBDwm7b3194F-Kd2Ya6xPMs3voDNg3t6QYC6VoSB4GbX8Tu24mqsI1g-ys50OCeuZgKl_vNjzl4xxDULG1t6ajIYrgyk_yoJKoAdoZUdaxQP8n2q3D58aP5iqAaRkqPl16aKytQvpRYsbpf_YxOAALSBroX6UddfZTXrsmE7I9coEQds3IAHymbXprJaWEUuccgBEjGH9KuAU_ohzb9QmsasjwGn75tSx_dZiA1wW453frxsIoQSce9Xs.khV5L7ej9RipuCNPJGIxyWO62xsjegWDLbC21s7Wsl4&dib_tag=se&keywords=jogo%2Btoalha%2Bde%2Bbanho&qid=1772414996&sprefix=jogo%2Btoalha%2Caps%2C269&sr=8-9&ufe=app_do%3Aamzn1.fos.6121c6c4-c969-43ae-92f7-cc248fc6181d&th=1"
     },
     {
         Nome: "Lixeira Banheiro",
-        imagem: "https://images.unsplash.com/photo-1620624970621-360216447883?w=500",
-        url: "https://www.google.com/search?q=lixeira+banheiro"
+        imagem: "https://m.media-amazon.com/images/I/41M8clT1JFL._AC_SL1280_.jpg",
+        url: "https://www.amazon.com.br/Lixeira-Redonda-Metal-Litros-Branco/dp/B0FVY7L8FX/ref=sr_1_20?crid=O1RTVBUOL9XI&dib=eyJ2IjoiMSJ9.TAhHQJ6dcky070P8CSE4el3Vt0oT03b3ieXnTSl7AClC4w3RvhO_NH0XpHXf7p8VFyAxAcmW937WZiWIL_yaFXM9cGEdCtYx19-MDZS1kL50lhCF2MMuoh_cmoli1IdpWaTalA2nPu7LGwb40xRm-fofObPe4wJcRBjlOMvyRXjimyt9Z-XFhp1Jp0FGwGLgzZmo-js1b-DfcCUc2g62hB0sCwA6Muv9I5Pfuyx8q2wztNzbE1Re_0qj5t-Y7ZKvUXKiUXucogB0QCP2YEIxAqNzkITw3jfdjDWwxylivGU._7khVqgYdP8qy8TpRgaeirpPbQHvn_Nr6pf23NS5mZk&dib_tag=se&keywords=lixeira%2Bbanheiro&qid=1772415095&sprefix=Lixeira%2B%2Caps%2C266&sr=8-20&ufe=app_do%3Aamzn1.fos.6d798eae-cadf-45de-946a-f477d47705b9&th=1"
     },
     {
         Nome: "Kit 2 Canecas de Chopp",
-        imagem: "https://images.unsplash.com/photo-1538481199705-c710c4e965fc?w=500",
-        url: "https://www.google.com/search?q=caneca+de+chopp"
+        imagem: "https://m.media-amazon.com/images/I/61w4OgN-d+L._AC_SL1200_.jpg",
+        url: "https://www.amazon.com.br/Kit-Canecas-Chopp-350ml-Vidro/dp/B0FW5HXHQH/ref=sr_1_7?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=37KXCAR894CGY&dib=eyJ2IjoiMSJ9.IOr7HqJm6psOP5LOhRneaz3yRCdXt16c3Uaz9VoBhb9rnGcLHnkclJiRAI3DdjcOSHosrRFUzZbNvZuixvuiOMknJFvIaCNVD26yLAg2iVh7rHwgpsNfrVtjq88TUetmnYCkRaOjoP_Vb5prfNzl75LTsHVRNpUWa2GGWc1wz-i8zfOLpy_SRYv1P5iFMQMNELxl0AUWFprR-rrF0tgipKJlX9h4q9CjuNC690LvGfsPBmd1ec9HwnQsmhuiVVrzRsyJRnTB8YrTGeZFlwcFRX1e_aFo3hB-3JP45f4392o.hjA7Mr5piqK5auXzbs6jEcMpCBMWn3nJkfjXf1rdSnc&dib_tag=se&keywords=kit+caneca+de+chopp&qid=1772415170&sprefix=kit+caneca+de+chop%2Caps%2C261&sr=8-7"
     },
     {
-        Nome: "Escorredor de Macarrão",
-        imagem: "https://images.unsplash.com/photo-1594833134105-09673891795c?w=500",
-        url: "https://www.google.com/search?q=escorredor+de+macarrao"
-    },
-    {
-        Nome: "Cuscuzeira",
-        imagem: "https://images.unsplash.com/photo-1585231007224-3711b9340685?w=500",
-        url: "https://www.google.com/search?q=cuscuzeira+inox"
+        Nome: "TV",
+        imagem: "https://m.media-amazon.com/images/I/81-ljI2R4dL._AC_SL1500_.jpg",
+        url: "https://www.amazon.com.br/Samsung-Smart-Crystal-U8100F-2025/dp/B0F5X69KQB/ref=sr_1_6?crid=2MZC61R2DZ6VB&dib=eyJ2IjoiMSJ9.70F1TS-WLRCxC7lre_T9AYFU1jMagz9HLIvGFdYMMoDT-ChLgEw-vb16_4Q-yw7T0QsCPUOoVPLQjKBrfLXvkXe7sc1Zoa_uKkobQPeulnpoYxsW5fx8N7X8thFlg52jbfX3HYewQOcJ7VnPu1hTMEDPIL6bp-TXH01j1TNA1cfRCB6h0nMdU76oYfOK70XljbqiLttgtPyOT-e3FTPaeWApV1pAnNLpYyBuPiB1mYyKOQqN45i-OmGuzWU4r82Q9N6FBEv0mkmd1PGFKbS5skY1wi_oaFMwJY3bqRQU4ws.FXSuOaYI5P52JGqTSRX6z9A-J66fQDbTwIKUOZhVntE&dib_tag=se&keywords=televis%C3%A3o%2B43%2Bpolegadas&qid=1772415053&sprefix=televisao%2B43%2Caps%2C264&sr=8-6&th=1"
     }
 ];
 
@@ -116,7 +111,7 @@ function carregarItens() {
         card.innerHTML = `
             <img src="${item.imagem}" alt="${item.Nome}">
             <h3>${item.Nome}</h3>
-            <span class="btn-comprar">Ver Opções</span>
+            <span class="btn-comprar">Ver sgestão</span>
             <p class="note">*site de compra é apenas uma sugestão</p>
         `;
 
